831a6eab69e8ab40ee7a62885fb6e7a0f2d9ab2d
ZouBaoni <baoni.25@intl.zju.edu.cn>
2026-03-11 07:49:18 +0100

    add lines to print the results of d, e and W

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -4,6 +4,10 @@ c = 5.55 #the estimated population in Scotland (million) in 2024
 d = b - a #the change in population between 2014 and 2004
 e = c - b #the change in population between 2024 and 2014
 
+#print d and e
+print ("d = " + str(d))
+print ("e = " + str(e))
+
 #compare d to e and output
 if d > e:
     print ("d > e") 
@@ -16,6 +20,8 @@ else:
 X = True #initialise X
 Y = False #initialise Y
 W = X or Y #calculate the result
+
+print ("W = " + str(W)) #output for the result
 
 #the truth table for W
 # X     Y     W(X or Y)
